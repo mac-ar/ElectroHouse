42f8728c6718cbba58ffc87e34ae29011b483e31
dmorasole <dmorasole@gmail.com>
2023-11-07 18:02:03 -0300

    app.js arreglo re ruta

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'))
-})
-app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'views', 'Registro.html'))
+/* RUTAS */
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "src", 'views', 'index.html'))
 })
 
 const port = 3000;
