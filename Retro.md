78d0fcd0de4fd8480429868e31a092b6965e312c
dmorasole <dmorasole@gmail.com>
2023-11-07 17:49:55 -0300

    ruta en app.js + retro

--- Retro.md
+++ Retro.md
@@ -1,2 +1,6 @@
 ## Spring UNO
-Hola
+
+Sprint 1 - Principales conclusiones:
+Como conclusion podemos decir que es muy importante el trabajo en equipo, 
+asi tambien como es muy necesario oraganizarce en las tareas y los tiempos en que van a demandar esas tareas. 
+Cuando todos hacen una parte en tiempo y forma se puede logar el objetivo.
--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-const port = 3000;
 
-app.listen(port, ()=> console.log(`Servidor Ejecuntandose en puerto: ${port}`))
-
-/* RUTAS */
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'src/views', 'index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'))
+})
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'views', 'Registro.html'))
 })
 
+const port = 3000;
+app.listen(port, () => console.log(`Servidor Ejecuntandose en puerto: ${port}`))
