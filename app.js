f7603a88f86dca5450034b01e525fa708ed0f256
dmorasole <dmorasole@gmail.com>
2023-11-07 18:30:48 -0300

    producto + app.js

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(express.static(path.join(__dirname, 'src', 'public')));
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, "src", 'views', 'index.html'))
 })
+app.get('/ProductoDetalle', (req,res)=>{
+    res.sendFile(path.join(__dirname, "src", 'views', 'ProductoDetalle.html'))
+})
+app.get('/Registro', (req,res)=>{
+    res.sendFile(path.join(__dirname, "src", 'views', 'Registro.html'))
+})
 
 const port = 3000;
 app.listen(port, () => console.log(`Servidor Ejecuntandose en puerto: ${port}`))
