d7cd3ff1fcded33fa72b5a417b2078ba29db9c9d
Roque Peralta <pitty4102@gmail.com>
2023-12-09 13:17:17 -0300

    corregi los indexControl y carrito

--- src/controllers/indexControl.js
+++ src/controllers/indexControl.js
@@ -4,8 +4,10 @@ const path = require('path')
 const pathProductos = path.join(__dirname, '../data/listaProductos.json')
 const listaProductos = JSON.parse(fs.readFileSync(pathProductos, 'utf-8'))
 
-const indexControl = {
+const indexController = {
     index: (req, res) => {
         res.render('index', { listaProductos })
     }
 }
+
+module.exports = indexController;
